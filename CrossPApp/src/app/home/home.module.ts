76ba089f84fa3bc3d81924a422a7358b2ccedc97
Tilak <aruntilak2@gmail.com>
2020-04-10 20:50:45 -0700

    Home Page Added

--- CrossPApp/src/app/home/home.module.ts
+++ CrossPApp/src/app/home/home.module.ts
@@ -3,17 +3,17 @@ import { RouterModule } from '@angular/router';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { Tab1Page } from './tab1.page';
+import { HomePage } from './home.page';
 import { ExploreContainerComponentModule } from '../explore-container/explore-container.module';
 
 @NgModule({
-  imports: [
-    IonicModule,
-    CommonModule,
-    FormsModule,
-    ExploreContainerComponentModule,
-    RouterModule.forChild([{ path: '', component: Tab1Page }])
-  ],
-  declarations: [Tab1Page]
-})
-export class Tab1PageModule {}
+    imports: [
+      IonicModule,
+      CommonModule,
+      FormsModule,
+      ExploreContainerComponentModule,
+      RouterModule.forChild([{ path: '', component: HomePage }])
+    ],
+    declarations: [HomePage]
+  })
+  export class HomePageModule {}
